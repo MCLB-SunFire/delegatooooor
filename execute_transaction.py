2958a9c22e0c97552c983c115c7845a55191a71b
sunfire <you@example.com>
2025-03-24 12:55:15 -0700

    fixed actual problem of the busted tx soniscan links

--- execute_transaction.py
+++ execute_transaction.py
@@ -180,7 +180,8 @@ def execute_transaction(transaction):
             signed_tx = web3.eth.account.sign_transaction(tx, PRIVATE_KEY)
             tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
 
-            return tx_hash.hex()
+            return web3.to_hex(tx_hash)
+        
         except Exception as e:
             attempt += 1
             print(f"Error executing transaction (Attempt {attempt}/{max_retries}): {e}")
